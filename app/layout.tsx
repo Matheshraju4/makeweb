31f6a602af4b5b627e09a6a5fc1190a5d97c0acd
Matheshraju <101191899+Matheshraju4@users.noreply.github.com>
2026-05-09 11:50:11 +0530

    Add next-plausible for analytics and update layout to include provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,33 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import PlausibleProvider from "next-plausible";
 
 const outfit = Outfit({
-  subsets: ["latin"],
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "MakeWeb — Under Development",
-  description:
-    "MakeWeb is crafting something great. Our website is under development.",
-  icons: {
-    icon: "/logo.png",
-  },
+	title: "MakeWeb — Under Development",
+	description:
+		"MakeWeb is crafting something great. Our website is under development.",
+	icons: {
+		icon: "/logo.png",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${outfit.className} `}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${outfit.className} `}>
+				<PlausibleProvider src="https://plausible.io/js/pa-GK2saGpXluC-ZUUi4WAwe.js">
+					{children}
+				</PlausibleProvider>
+			</body>
+		</html>
+	);
 }
